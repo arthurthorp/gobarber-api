3b7ad589c362a4a46a814bf78c4f34c3ed38503d
Arthur Tavares <arthurthorp@hotmail.com>
2020-09-15 20:21:05 -0300

    Inicio da implementação de SOLID e DDD

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 import multer from 'multer';
-import uploadConfig from '../config/upload';
+import uploadConfig from '@config/upload';
 
-import CreateUserService from '../services/CreateUserService';
-import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+import CreateUserService from '@modules/users/services/CreateUserService';
+import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
 
 import ensureAuthenticated from '../middlewares/ensureAutheticated';
 
@@ -42,7 +42,6 @@ usersRouter.patch(
     delete user.password;
 
     return response.json(user);
-    return response.json({ ok: true });
   },
 );
 export default usersRouter;
